8255b599d2c9fc2af5aca446ba90aebddb7b66f6
Matt Ward <matthewward290@gmail.com>
2026-03-04 12:29:59 +0000

    Initialisation of AnswerSubmitController

---
+++ backend/src/Controller/Answer/AnswerSubmitController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Answer;
+
+use App\Domain\AssessmentAnswer;
+use App\Domain\AssessmentInstance;
+use App\Domain\AssessmentAnswerOption;
+use App\Domain\AssessmentQuestion;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnswerSubmitController extends AbstractController
+{
+    //to do
+}
